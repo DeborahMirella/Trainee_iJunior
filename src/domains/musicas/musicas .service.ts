ad488496166fce22baa01cf2a0e0156ffd4645d3
Arthur Vilas <arthurfelipevilasnovas@gmail.com>
2025-08-02 00:39:29 +0100

    feature: adiciona funcionalidade create do CRUD no service. Adiciona campo de duracao no model Musica.

--- src/domains/musicas/musicas .service.ts
+++ src/domains/musicas/musicas .service.ts
@@ -1,5 +1,31 @@
+import { Musica } from "@prisma/client";
 import prisma from "../../../config/client";
+import { Param } from "@prisma/client/runtime/library";
 
 class MusicService {
-  //Create - Criação de uma nova música.
+  //Create - Criação de uma nova música
+  async criarMusica(body: Omit<Musica, "id">, artistaId: number) {
+    if (!body.nome || !body.duracao || !body.artistaId) {
+      throw new Error(
+        "O nome, a duração e o id do artista não podem ser nulos."
+      );
+    }
+
+    const musicaCriada = await prisma.musica.create({
+      data: {
+        nome: body.nome,
+        duracao: body.duracao,
+        album: body.album,
+        genero: body.genero,
+        artista: {
+          connect: {
+            id: artistaId,
+          },
+        },
+      },
+      include: { artista: true },
+    });
+
+    return musicaCriada;
+  }
 }
